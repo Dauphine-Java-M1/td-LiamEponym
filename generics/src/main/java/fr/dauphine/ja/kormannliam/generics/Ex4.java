7967ddd94d1dee0e09d66592dc779abb697cf773
kormli18 <kormli18@b028-13.crio.dauphine.fr>
2019-12-09 14:16:52 +0100

    Correction ex4

--- generics/src/main/java/fr/dauphine/ja/kormannliam/generics/Ex4.java
+++ generics/src/main/java/fr/dauphine/ja/kormannliam/generics/Ex4.java
@@ -2,6 +2,8 @@ package fr.dauphine.ja.kormannliam.generics;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Ex4 {
@@ -16,11 +18,15 @@ public class Ex4 {
 		List<?> r4 = fusion(l1,l3);
 	}
 	
-	public static List<? extends Object> fusion(List l1, List l2){
-		List<?> res = new ArrayList<?>();
-		if (l1.size() == l2.size()) {
-			
+	public static <T> List<T> fusion(List<? extends T> l1, List<? extends T> l2){
+		List<T> lfusion = new LinkedList<T>();
+		Iterator<? extends T> it1 = l1.iterator();//? extends T important pour pas avoir à caster
+		Iterator<? extends T> it2 = l2.iterator();
+		while(it1.hasNext()) {
+			lfusion.add(it1.next());
+			lfusion.add(it2.next());
 		}
+		return lfusion;
 	}
 	
 }
